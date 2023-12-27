ee10b86ae85a574416fc3986db0f09fc6269674d
NatolokaS <129997302+NatolokaS@users.noreply.github.com>
2023-12-27 22:27:48 +0200

    finalversion

--- index.js
+++ index.js
@@ -32,9 +32,6 @@ async function invokeAction({ action, id, name, email, phone }) {
 
 invokeAction(argv);
 
-//
-// const { Command } = require('commander');
-// const program = new Command();
 program
   .option('-a, --action <type>', 'choose action')
   .option('-i, --id <type>', 'user id')
@@ -43,31 +40,3 @@ program
   .option('-p, --phone <type>', 'user phone');
 
 program.parse(process.argv);
-
-// const argv = program.opts();
-
-// // TODO: рефакторити
-// function invokeAction({ action, id, name, email, phone }) {
-//   switch (action) {
-//     case 'list':
-//       // ...
-//       break;
-
-//     case 'get':
-//       // ... id
-//       break;
-
-//     case 'add':
-//       // ... name email phone
-//       break;
-
-//     case 'remove':
-//       // ... id
-//       break;
-
-//     default:
-//       console.warn('\x1B[31m Unknown action type!');
-//   }
-// }
-
-// invokeAction(argv);
